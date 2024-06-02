8e38056a07d2cb8e79855d55fdcc9f00d56275dc
emna dammak <emna.dammak@insat.ucar.tn>
2024-06-02 11:03:22 -0400

    updated backend

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
-    resource_group_name  = "servicecom"
-    storage_account_name = "servicecom"
+    resource_group_name  = "automation"
+    storage_account_name = "tfstatesac"
     container_name       = "tfstate"
     key                  = "terraform.tfstate"
   }
