5ec5db0db0863f3130652f93717774ad12036a9e
AzizBenDhiab <mohamedaziz.bendhiab@insat.ucar.tn>
2024-06-03 19:24:16 +0100

    fix server-cmds script

--- server-cmds.sh
+++ server-cmds.sh
@@ -1,6 +1,6 @@
 
-export IMAGE_FRONT=$1
-export IMAGE_BACK=$2
+export IMAGE_FRONT=$2
+export IMAGE_BACK=$1
 export DOCKER_USER=$3
 export DOCKER_PWD=$4
 echo $DOCKER_PWD | docker login -u $DOCKER_USER --password-stdin
