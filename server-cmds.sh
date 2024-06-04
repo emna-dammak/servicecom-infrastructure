4397a9e0d234d843f509a8aef8a773fb9a10d542
AzizBenDhiab <mohamedaziz.bendhiab@insat.ucar.tn>
2024-06-05 00:06:49 +0100

    update server cmds

--- server-cmds.sh
+++ server-cmds.sh
@@ -4,5 +4,6 @@ export IMAGE_BACK=$1
 export DOCKER_USER=$3
 export DOCKER_PWD=$4
 echo $DOCKER_PWD | docker login -u $DOCKER_USER --password-stdin
+docker-compose pull
 docker-compose -f docker-compose.yaml up --detach
 echo "success"
